2089e059f6bb66b6b97e157b90ba601e017bd685
Terrence <yl124@rice.edu>
2018-06-27 00:54:42 -0500

    config

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./dist/index.html"
+});
+
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    // host: '0.0.0.0',
+    disableHostCheck: true,
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          plugins: ['transform-class-properties']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+  output: {
+    filename: './dist/[name].bundle.js',
+    path: path.resolve(__dirname,)
+  }
+};
